464ff02e83c7d2d8a291f0e77b5fdd29b183c642
Fushigo <rizky23ska@gmail.com>
2025-01-17 16:43:28 +0700

    update

--- src/routes/karyawanRoutes.ts
+++ src/routes/karyawanRoutes.ts
@@ -12,10 +12,10 @@ export default function karyawanRoutes() {
   const router = express.Router();
 
   router.get("/", getAllKaryawan);
-  router.get(":/id", getKaryawanById);
+  router.get("/:id", getKaryawanById);
   router.post("/", createKaryawan);
-  router.patch("/:id", updateKaryawan);
   router.patch("/nmkaryawan/:nomor", updateKaryawanByNomorKaryawan);
+  router.patch("/:id", updateKaryawan);
   router.delete("/:id", deleteKaryawan);
 
   return router;
